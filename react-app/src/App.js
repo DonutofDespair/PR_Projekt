df20a8995f524c42e5eef5be39ddfbbcc9a59708
DonutofDespair <mrkonrad300@gmail.com>
2023-12-18 19:31:27 +0100

    Komponenty Details

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,37 +1,73 @@
 // App.jsx
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
 import Registration from './Registration';
 import Login from './Login';
-import Footer from './Footer'; // Dodaj import
+import Footer from './Footer';
+import Details from './Details';
 
-const Home = ({ movies }) => (
+// Rename the functional component to MovieDetail
+const MovieDetail = ({ movie }) => (
   <div>
-    <h2>Home</h2>
-    <div id="search-results" className="movie-tiles">
-      {movies.map((movie, index) => (
-        <div key={index} className="movie-tile">
-          <img src={movie.image} alt={movie.title} />
-          <h3>{movie.title}</h3>
-          <p>{movie.description}</p>
-          <div className="rating">
-            {Array.from({ length: movie.rating }, (_, i) => (
-              <span key={i}>&#9733;</span>
-            ))}
-          </div>
-        </div>
+    <h2>{movie.title}</h2>
+    <img src={movie.image} alt={movie.title} />
+    <p>{movie.description}</p>
+    <div className="rating">
+      {Array.from({ length: movie.rating }, (_, i) => (
+        <span key={i}>&#9733;</span>
       ))}
     </div>
   </div>
 );
 
-const Details = () => (
+const Home = ({ movies }) => (
   <div>
-    <h2>Details</h2>
-    {/* Place the content of the details page here */}
+    <div id="search-results" className="movie-tiles">
+      {movies.map((movie, index) => (
+        <Link key={index} to={`/details/${index}`} className="movie-tile-link">
+          <div className="movie-tile">
+            <img src={movie.image} alt={movie.title} />
+            <h3>{movie.title}</h3>
+            <p>{movie.description}</p>
+            <div className="rating">
+              {Array.from({ length: movie.rating }, (_, i) => (
+                <span key={i}>&#9733;</span>
+              ))}
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
   </div>
 );
+
+// Rename the JSX element to DetailsPage
+const DetailsPage = ({ params }) => {
+  const movieIndex = params.index;
+  const movies = [
+    { title: 'Film 1', image: '/image2.jpg', description: 'This is the first movie about...', rating: 4 },
+    { title: 'Film 2', image: '/image2.jpg', description: 'This is the second movie about...', rating: 5 },
+    { title: 'Film 3', image: '/image2.jpg', description: 'This is the third movie about...', rating: 3 },
+    { title: 'Film 4', image: '/image2.jpg', description: 'This is the fourth movie about...', rating: 4 },
+    { title: 'Film 5', image: '/image2.jpg', description: 'This is the fifth movie about...', rating: 2 },
+    { title: 'Film 6', image: '/image2.jpg', description: 'This is the sixth movie about...', rating: 5 },
+    { title: 'Film 7', image: '/image2.jpg', description: 'This is the seventh movie about...', rating: 3 },
+    { title: 'Film 8', image: '/image2.jpg', description: 'This is the eighth movie about...', rating: 4 },
+    { title: 'Film 9', image: '/image2.jpg', description: 'This is the ninth movie about...', rating: 1 },
+    { title: 'Film 10', image: '/image2.jpg', description: 'This is the tenth movie about...', rating: 5 },
+    { title: 'Film 11', image: '/image2.jpg', description: 'This is the eleventh movie about...', rating: 2 },
+    { title: 'Film 12', image: '/image2.jpg', description: 'This is the twelfth movie about...', rating: 4 },
+    { title: 'Film 13', image: '/image2.jpg', description: 'This is the thirteenth movie about...', rating: 3 },
+    { title: 'Film 14', image: '/image2.jpg', description: 'This is the fourteenth movie about...', rating: 5 },
+    { title: 'Film 15', image: '/image2.jpg', description: 'This is the fifteenth movie about...', rating: 4 },
+  ];
+
+  const movie = movies[movieIndex];
+
+  return <MovieDetail movie={movie} />;
+};
 
 const AddMovie = () => (
   <div>
@@ -73,35 +109,39 @@ const SearchBar = () => (
 
 const App = () => {
   const movies = [
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
-    { title: 'Film', image: 'image2.jpg', description: 'This is a great movie about...', rating: 5 },
+    { title: 'Film 1', image: '/image2.jpg', description: 'This is the first movie about...', rating: 4 },
+    { title: 'Film 2', image: '/image2.jpg', description: 'This is the second movie about...', rating: 5 },
+    { title: 'Film 3', image: '/image2.jpg', description: 'This is the third movie about...', rating: 3 },
+    { title: 'Film 4', image: '/image2.jpg', description: 'This is the fourth movie about...', rating: 4 },
+    { title: 'Film 5', image: '/image2.jpg', description: 'This is the fifth movie about...', rating: 2 },
+    { title: 'Film 6', image: '/image2.jpg', description: 'This is the sixth movie about...', rating: 5 },
+    { title: 'Film 7', image: '/image2.jpg', description: 'This is the seventh movie about...', rating: 3 },
+    { title: 'Film 8', image: '/image2.jpg', description: 'This is the eighth movie about...', rating: 4 },
+    { title: 'Film 9', image: '/image2.jpg', description: 'This is the ninth movie about...', rating: 1 },
+    { title: 'Film 10', image: '/image2.jpg', description: 'This is the tenth movie about...', rating: 5 },
+    { title: 'Film 11', image: '/image2.jpg', description: 'This is the eleventh movie about...', rating: 2 },
+    { title: 'Film 12', image: '/image2.jpg', description: 'This is the twelfth movie about...', rating: 4 },
+    { title: 'Film 13', image: '/image2.jpg', description: 'This is the thirteenth movie about...', rating: 3 },
+    { title: 'Film 14', image: '/image2.jpg', description: 'This is the fourteenth movie about...', rating: 5 },
+    { title: 'Film 15', image: '/image2.jpg', description: 'This is the fifteenth movie about...', rating: 4 },
   ];
+
 
   return (
     <Router>
       <div className="App">
         <header className="App-header">
-          <div className="logo-search">
-            <div className="logo">
-              <Link to="/">
-                <img src="Logo_Klapka.png" alt="Movie World Logo" />
-              </Link>
-            </div>
-            <div className="title-container">
-              <Link to="/">
-                <h1 className="title">Movie World</h1>
-              </Link>
-            </div>
-            <SearchBar />
+          <div className="logo">
+            <Link to="/">
+              <img src="./Logo_Klapka.png" alt="Movie World Logo" />
+            </Link>
           </div>
+          <div className="title-container">
+            <Link to="/">
+              <h1 className="title">Movie World</h1>
+            </Link>
+          </div>
+          <SearchBar />
           <div id="login">
             <Link to="/login" className="signin-button">
               Sign In
@@ -115,14 +155,14 @@ const App = () => {
         <div className="container">
           <Routes>
             <Route path="/" element={<Home movies={movies} />} />
-            <Route path="/details" element={<Details />} />
+            <Route path="/details/:index" element={<Details />} />
             <Route path="/add" element={<AddMovie />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/signup" element={<Registration />} />
             <Route path="/login" element={<Login />} />
           </Routes>
         </div>
-        <Footer /> {/* Dodaj stopkę na dole strony */}
+        <Footer />
       </div>
     </Router>
   );
